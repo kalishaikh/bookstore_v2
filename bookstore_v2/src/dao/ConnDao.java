6f403103f40935020ce964d8047bb63e9ec79aaf
Kaasim Shaikh <53850980+kalishaikh@users.noreply.github.com>
2021-03-25 13:20:12 -0400

    Merged all branches from old GitHub into Develop

--- bookstore_v2/src/dao/ConnDao.java
+++ bookstore_v2/src/dao/ConnDao.java
@@ -1,8 +1,24 @@
 package dao;
 
+import java.sql.*;
+
 public class ConnDao {
 
 	public static void main (String [] args) {
 		
+		try {
+		
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		Connection con = DriverManager.getConnection("jdbc:mysql://us-cdbr-east-03.cleardb.com/heroku_e71303011de1bce", "bbb09bc37f79b0", "7c9226ac");
+		Statement stmt=con.createStatement();
+		System.out.println("connected");
+		ResultSet rs=stmt.executeQuery("select * from test");  
+		while(rs.next())  
+		System.out.println(rs.getInt(1)+"  "+rs.getString(2));  
+		con.close();
+		} catch(Exception e) {
+			System.out.println(e);
+			
+		}
 	}
 }
