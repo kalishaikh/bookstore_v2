639208321b57e33be0a3c8ef47176bd6e37479dc
ngov94 <neena.govindhan@yahoo.ca>
2021-04-09 08:02:43 -0400

    Added Partner button on main page and added missing dependancy to pom file

--- bookstore_v2/src/rest/Book.java
+++ bookstore_v2/src/rest/Book.java
@@ -6,6 +6,8 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
 import model.BookModel;
 
 @Path("Book")
@@ -13,7 +15,7 @@ public class Book {
 
 	@GET
 	@Path("/read/")
-	@Produces("text/plain")
+	@Produces(MediaType.TEXT_PLAIN)
 	public String getStudent(@QueryParam("productId") String isbn) throws Exception {
 		System.out.println("Received: "+isbn);
 		return BookModel.getInstance().exportJSON(isbn);
